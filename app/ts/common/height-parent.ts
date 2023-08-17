2903970c105e1acf111db9f5e12257f1e485ac49
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-08-17 11:54:22 +0300

    fixed initHeightParent

--- app/ts/common/height-parent.ts
+++ app/ts/common/height-parent.ts
@@ -1,5 +1,3 @@
 export default function initHeightParent(parent: HTMLElement) {
-    console.log(parent.clientHeight);
-    
-    // parent.style.height = parent.clientHeight + "px";
+    parent.style.height = parent.clientHeight + "px";
 }
